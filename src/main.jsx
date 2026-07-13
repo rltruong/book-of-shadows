a90e34b1d61e5e6e4f709fc288a7eba2c9199f06
Richard Truong <rltruong@gmail.com>
2026-07-13 06:30:55 -0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import '@fontsource/manrope/latin-400.css';
-import '@fontsource/manrope/latin-500.css';
-import '@fontsource/manrope/latin-600.css';
-import '@fontsource/manrope/latin-700.css';
+import '@fontsource/inter/latin-400.css';
+import '@fontsource/inter/latin-500.css';
+import '@fontsource/inter/latin-600.css';
+import '@fontsource/inter/latin-700.css';
 import './index.css';
 import AuthGate from './AuthGate.jsx';
 import App from './App.jsx';
